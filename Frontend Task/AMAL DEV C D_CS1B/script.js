9b9ddf24102434dcad5a4954594ab39941832637
AMAL <mailtoamaldevcd@gmail.com>
2021-03-24 23:40:14 +0530

    numbers

--- Frontend Task/AMAL DEV C D_CS1B/script.js
+++ Frontend Task/AMAL DEV C D_CS1B/script.js
@@ -22,3 +22,20 @@ $(document).ready(function(){
        $('.back-to-top').tooltip('show');
 
 });
+
+// Counter 
+$(document).ready(function() {
+
+    $('.counter').each(function () {
+    $(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+    }, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+    $(this).text(Math.ceil(now));
+    }
+    });
+    });
+    
+    });
